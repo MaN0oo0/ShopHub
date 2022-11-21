cc08714b6e5e6a1315648686eb6a437d100e4986
MaN0oo0 <orbanm316@gmail.com>
2022-11-21 21:35:07 +0200

    Second commit

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 let btnserach=document.getElementById("serach");
 let showSerach=document.getElementById("showSerach");
-let btnadd=document.getElementById("btn-add");
+let btnadd=document.querySelector(".Catogray");
 
 
 
@@ -12,8 +12,4 @@ btnserach.onclick=function(){
     this.classList.add("d-none");
 showSerach.classList.remove("d-none")
 showSerach.innerHTML="<div class='input-group'> <input type='text'> <button class='btn btn-primary'>Serach</button>";
-}
-
-btnadd.onclick=function(){
-    alert("aaaaaaaaaa");
 }
