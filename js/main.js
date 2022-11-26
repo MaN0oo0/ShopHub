9440b41ef33afb61570009002b2b4e942d04e499
MaN0oo0 <orbanm316@gmail.com>
2022-11-26 21:26:16 +0200

    add ProductGetApi commit

--- js/main.js
+++ js/main.js
@@ -13,3 +13,21 @@ btnserach.onclick=function(){
 showSerach.classList.remove("d-none")
 showSerach.innerHTML="<div class='input-group'> <input type='text'> <button class='btn btn-primary'>Serach</button>";
 }
+
+
+let toggler=document.querySelector(".toggle");
+let header=document.querySelector(".header");
+let close=document.querySelector(".close");
+let btn_order=document.querySelector(".btn-order");
+
+
+
+toggler.onclick=function(){
+    header.classList.add("open");
+}
+close.onclick=function(){
+    header.classList.remove("open");
+}
+btn_order.onclick=function(){
+    header.classList.remove("open");
+}
